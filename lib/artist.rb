e4e814daa2c1ba78394ce279b6e58557490d5896
KelsCree <kelseycreehan@gmail.com>
2020-08-13 15:55:17 -0600

    Add additional class methods.

---
+++ lib/artist.rb
@@ -0,0 +1,25 @@
+require 'pry'
+class Artist
+    attr_accessor :name
+    @@all = []
+    def initialize name
+        @name = name
+        @@all << self
+    end
+
+    def songs
+         Song.all.select { |song| song.artist == self}
+    end
+
+    def  add_song(song)
+        song.artist == self
+    end
+
+    def add_song_by_name(song_name)
+        Song.new(song_name, self)
+    end
+
+    def self.song_count
+        all.length
+    end
+end
